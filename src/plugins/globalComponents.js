4058f89c3059b22179a3b949e983c6081750d0ab
moonsungwan <grape0122@naver.com>
2022-08-10 19:09:34 +0900

    global component 규격화

--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,13 +1,27 @@
-import TheHeader from '@/components/common/TheHeader';
-import BlogTable from '@/components/BlogTable';
-import BookmarkTable from '@/components/BookmarkTable';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
 import Pagination from 'vue-pagination-2';
 
 export default {
     install(Vue) {
-        Vue.component(TheHeader.name, TheHeader);
-        Vue.component(BlogTable.name, BlogTable);
-        Vue.component(BookmarkTable.name, BookmarkTable);
+        const requireComponent = require.context(
+            '/src/components/common',
+            false,
+            /(Base|The)[A-Z]\w+\.(vue|js)$/,
+        );
+        requireComponent.keys().forEach(fileName => {
+            const componentConfig = requireComponent(fileName);
+            const componentName = upperFirst(
+                camelCase(
+                    fileName
+                        .split('/')
+                        .pop()
+                        .replace(/\.\w+$/, ''),
+                ),
+            );
+            Vue.component(componentName, componentConfig.default || componentConfig);
+        });
+
         Vue.component('pagination', Pagination);
     },
 };
